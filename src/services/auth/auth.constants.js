4d382b84510d30903cf1a017a2a91e4013de8258
Pedrohmedd <pedromedeirosetec02@gmail.com>
2025-10-05 19:14:28 -0300

    zz

--- src/services/auth/auth.constants.js
+++ src/services/auth/auth.constants.js
@@ -1,5 +1,5 @@
 // URL base da sua API PHP
-export const API_BASE_URL = 'http://localhost/api-php';
+export const API_BASE_URL = 'http://localhost/back-end/api-php';
 
 // Endpoints da API
 export const API_ENDPOINTS = {
